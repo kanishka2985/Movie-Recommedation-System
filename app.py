93958618119cf5996cbf9e082c83efa6d1c103a4
kanishka sharma <kanishkas2908@gmail.com>
2025-07-25 00:29:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,6 @@ def health():
 # Run
 # ------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
